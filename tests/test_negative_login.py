06369a938a40330565ef29f0e909784b35b9dff2
walidatorkh <walidatorkh@gmail.com>
2024-04-03 15:54:39 +0300

    first commit

---
+++ tests/test_negative_login.py
@@ -0,0 +1,44 @@
+from playwright.sync_api import expect, Page
+import pytest
+
+testing_data = [
+    ('locked_out_user', 'secret_sauce', 'Epic sadface: Sorry, this user has been locked out.'),
+    ('', '', 'Epic sadface: Username is required'),
+    ('DDDDD', '', 'Epic sadface: Sorry, Password is required'),
+    ('wrong_username', 'secret_sauce', 'Epic sadface: Username and password do not match any user in this service'),
+]
+
+
+@pytest.mark.parametrize("username, password, error_message", testing_data)
+def test_not_valid_login_csenarios(page: Page, username: str, password: str, error_message: str) -> None:
+    page.goto("https://www.saucedemo.com/")
+    page.locator("[data-test=\"username\"]").fill("locked_out_user")
+    page.locator("[data-test=\"password\"]").fill("secret_sauce")
+    page.locator("[data-test=\"login-button\"]").click()
+    expect(page.locator("[data-test=\"error\"]")).to_be_visible()
+    expect(page.locator("[data-test=\"error\"]")).to_contain_text(
+        "Epic sadface: Sorry, this user has been locked out.")
+    expect(page).to_have_url("https://www.saucedemo.com/")
+#     page.wait_for_timeout(3000)
+#
+#     def test_locked_out_user_login(page: Page) -> None:
+#         page.goto("https://www.saucedemo.com/")
+#         page.locator("[data-test=\"username\"]").fill("locked_out_user")
+#         page.locator("[data-test=\"password\"]").fill("secret_sauce")
+#         page.locator("[data-test=\"login-button\"]").click()
+#         expect(page.locator("[data-test=\"error\"]")).to_be_visible()
+#         expect(page.locator("[data-test=\"error\"]")).to_contain_text(
+#             "Epic sadface: Sorry, this user has been locked out.")
+#         expect(page).to_have_url("https://www.saucedemo.com/")
+#         page.wait_for_timeout(3000)
+#
+#
+# def test_login_with_empty_credentials(page: Page) -> None:
+#     page.goto("https://www.saucedemo.com/")
+#     page.locator("[data-test=\"username\"]").fill("")
+#     page.locator("[data-test=\"password\"]").fill("")
+#     page.locator("[data-test=\"login-button\"]").click()
+#     expect(page.locator("[data-test=\"error\"]")).to_be_visible()
+#     expect(page.locator("[data-test=\"error\"]")).to_contain_text("Epic sadface: Username is required")
+#     expect(page).to_have_url("https://www.saucedemo.com/")
+#     page.wait_for_timeout(3000)
